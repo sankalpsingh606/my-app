075e091554d5c7b9e9e6f4fba1ac9d02873d2d24
Sankalp Singh <sankalpsingh606@gmail.com>
2020-03-03 14:16:47 +0530

    add method

--- App.java
+++ App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
+        int a = 56;
+        int b = 43;
+        int c = a + b;
+        System.out.println("Result : " + c);
         System.out.println("Hello World");
     }
 }
